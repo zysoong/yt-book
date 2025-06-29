ce49a2edc1cd0c50305606e0eee1cd38daed0444
Ziyang SONG <ziyang.song@gmx.de>
2025-06-30 01:04:56 +0200

    draft

--- utils/yt_download.py
+++ utils/yt_download.py
@@ -1,5 +1,18 @@
-from yt_dlp import YoutubeDL
+import os
+from yt_dlp import YoutubeDL, DownloadError
+from yt_dlp.utils import ExtractorError
 
-URLS = ['https://www.youtube.com/watch?v=QAgR4uQ15rc&list=PLS01nW3RtgopsNLeM936V4TNSsvvVglLc']
-with YoutubeDL() as ydl:
-    ydl.download(URLS)
+base_dir = os.path.join("../", "downloaded_videos")
+ydl_opts = {
+    #"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best",
+    "outtmpl": os.path.join(base_dir, "%(playlist_title)s", "%(playlist_index)s - %(title)s.%(ext)s"),
+    "restrictfilenames": True
+}
+URLS = ["https://www.youtube.com/watch?v=QAgR4uQ15rc&list=PLS01nW3RtgopsNLeM936V4TNSsvvVglLc"]
+with YoutubeDL(ydl_opts) as ydl:
+    try:
+        ydl.download(URLS)
+    except ExtractorError as e:
+        print(e)
+    except DownloadError as e:
+        print(e)
