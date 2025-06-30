1542bf28592b90ababad24ded4ef0a1287f04a99
zysoong <ziyang.song@gmx.de>
2025-06-30 10:52:06 +0200

    Create downloader as utils

--- utils/yt_download.py
+++ utils/yt_download.py
@@ -1,18 +1,30 @@
 import os
+from typing import Any
+
 from yt_dlp import YoutubeDL, DownloadError
 from yt_dlp.utils import ExtractorError
 
-base_dir = os.path.join("../", "downloaded_videos")
-ydl_opts = {
-    #"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best",
-    "outtmpl": os.path.join(base_dir, "%(playlist_title)s", "%(playlist_index)s - %(title)s.%(ext)s"),
-    "restrictfilenames": True
-}
-URLS = ["https://www.youtube.com/watch?v=QAgR4uQ15rc&list=PLS01nW3RtgopsNLeM936V4TNSsvvVglLc"]
-with YoutubeDL(ydl_opts) as ydl:
-    try:
-        ydl.download(URLS)
-    except ExtractorError as e:
-        print(e)
-    except DownloadError as e:
-        print(e)
+
+def download_videos_from_playlist(playlist_url: str, is_best_quality: bool):
+    base_dir = os.path.join("../", "downloaded_videos")
+    ydl_opts_download: dict[str, Any] = {
+        "outtmpl": os.path.join(base_dir, "%(playlist_title)s", "%(playlist_index)s - %(title)s.%(ext)s"),
+        "restrictfilenames": True
+    }
+
+    if is_best_quality:
+        ydl_opts_download["format"] = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best"
+
+    with YoutubeDL(ydl_opts_download) as ydl:
+        try:
+            ydl.download(playlist_url)
+        except (ExtractorError, DownloadError) as e:
+            print(e)
+
+
+def extract_information_from_playlist(playlist_url: str) -> dict[str, Any]:
+    ydl_opts_extract_info = {
+        "ignoreerrors": True
+    }
+    with YoutubeDL(ydl_opts_extract_info) as ydl:
+        return ydl.extract_info(playlist_url, download=False)
