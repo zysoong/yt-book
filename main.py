c199303eadff245651c707acb7b6d9cb8d2d708a
zysoong <ziyang.song@gmx.de>
2025-07-06 19:27:37 +0200

    Add transcription

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 
 import os
 
+from utils.transcribe_audios import transcribe_mp3_folder
 from utils.video_audio_conversion import convert_to_mp3
 from utils.yt_download import download_from_youtube_url
 
@@ -9,9 +10,16 @@ from utils.yt_download import download_from_youtube_url
 async def main():
     base_dir_download: str = os.path.join("./", "downloaded_videos")
     base_dir_convert: str = os.path.join("./", "converted_audios")
+    base_dir_transcription: str = os.path.join("./", "transcriptions")
     url: str = "https://www.youtube.com/watch?v=QAgR4uQ15rc&list=PLS01nW3RtgopsNLeM936V4TNSsvvVglLc"
-    await download_from_youtube_url(url=url, output_dir=base_dir_download)
-    await convert_to_mp3(input_folder=os.path.join(base_dir_download, "NA"), output_folder=base_dir_convert)
+    #await download_from_youtube_url(url=url, output_dir=base_dir_download)
+    #await convert_to_mp3(input_folder=os.path.join(base_dir_download, "NA"), output_folder=base_dir_convert)
+    transcribe_mp3_folder(
+        input_folder=base_dir_convert,
+        output_folder=base_dir_transcription,
+        device="cuda",
+        compute_type="int8"
+    )
 
 
 if __name__ == "__main__":
---
+++ utils/transcribe_audios.py
@@ -0,0 +1,43 @@
+import json
+import os
+import torch
+from faster_whisper import WhisperModel, BatchedInferencePipeline
+
+
+def transcribe_mp3_folder(
+    input_folder: str,
+    output_folder: str,
+    model_size: str = "base",
+    device: str | None= None,
+    compute_type: str = "float16"
+) -> None:
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    os.makedirs(output_folder, exist_ok=True)
+    model = WhisperModel(model_size, device=device, compute_type=compute_type)
+    pipeline = BatchedInferencePipeline(model=model)
+
+    files_to_be_transcribed: list[str] = [
+        filename for filename in os.listdir(input_folder) if filename.lower().endswith(".mp3")
+    ]
+    for filename in files_to_be_transcribed:
+        input_path = os.path.join(input_folder, filename)
+        print(f"Transcribing {filename}...")
+
+        segments, info = pipeline.transcribe(input_path, beam_size=5)
+
+        output = {
+            "language": info.language,
+            "segments": [
+                {"start": seg.start, "end": seg.end, "text": seg.text}
+                for seg in segments
+            ]
+        }
+        output_filename = os.path.splitext(filename)[0] + ".json"
+        output_path = os.path.join(output_folder, output_filename)
+
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(output, f, indent=2)
+
+        print(f"Saved transcription to {output_path}")
